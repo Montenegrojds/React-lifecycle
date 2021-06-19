72cf2ffd106131e78082a8c4b5ba53fe1f950ba6
monte <montenegrojds@gmail.com>
2021-06-18 20:38:08 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import Counter from './components/counter'
+class App extends react.Component{
+  render(){
+    return(
+      <div>
+        <Counter />
+      </div>
+    )
+  }
+}
+
+export default App;
